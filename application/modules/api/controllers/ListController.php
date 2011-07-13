102e4b23803ec6bf83732516346af41cfa4bf01f
André Roaldseth <andrer@vg.no>
2011-07-13 13:58:59 +0200

    Make the API work again.

--- application/modules/api/controllers/ListController.php
+++ application/modules/api/controllers/ListController.php
@@ -3,15 +3,15 @@
 class Api_ListController extends Zend_Controller_Action {
 
     public function init() {
-        $this->_helper->layout->disableLayout();
+        #$this->_helper->layout->disableLayout();
+        $this->db = $this->getInvokeArg('bootstrap')->getResource('db');
     }
 
     public function indexAction() {
         if (!isset($_GET['server'])) {
             $this->view->response = array('success' => false, 'error' => 'Missing server');
         } else {
-            $pdo = $this->getInvokeArg('bootstrap')->getResource('pdo');
-            $cronjobList = new Application_Model_CronjobList($pdo);
+            $cronjobList = new Application_Model_CronjobList($this->db);
             $cronjobs = $cronjobList->getByServer($_GET['server']);
 
             $this->view->response = $cronjobs;
