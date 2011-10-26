33d6f7ff45263a075a60a6e694c6981153657f78
André Roaldseth <andrer@vg.no>
2011-10-26 11:23:12 +0200

    Add routes for the API-module.

--- server/application/modules/api/Bootstrap.php
+++ server/application/modules/api/Bootstrap.php
@@ -1,4 +1,40 @@
 <?php
 class Api_Bootstrap extends Zend_Application_Module_Bootstrap {
+    protected function _initRoutes() {
+        $router = $this->bootstrap('frontController')->getResource('frontController')->getRouter();
 
+        $routes = new Zend_Config(array(
+            'api' => array(
+                'type' => 'Zend_Controller_Router_Route_Static',
+                'route' => 'api',
+                'defaults' => array(
+                    'module' => 'api',
+                    'controller' => 'api',
+                    'action' => 'index',
+                ),
+                'chains' => array(
+                    'list' => array(
+                        'type' => 'Zend_Controller_Router_Route_Static',
+                        'route' => 'list',
+                        'defaults' => array(
+                            'module' => 'api',
+                            'controller' => 'list',
+                            'action' => 'index',
+                        ),
+                    ),
+                    'log' => array(
+                        'type' => 'Zend_Controller_Router_Route_Static',
+                        'route' => 'log',
+                        'defaults' => array(
+                            'module' => 'api',
+                            'controller' => 'log',
+                            'action' => 'index',
+                        ),
+                    ),
+                ),
+            ),
+        ));
+
+        $router->addConfig($routes);
+    }
 }
