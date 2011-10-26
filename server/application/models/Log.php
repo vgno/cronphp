3008024cc20923e5b889cace6bde3c2dc648446e
André Roaldseth <andrer@vg.no>
2011-10-26 16:00:06 +0200

    Make sure to sort the log entries descending by start time.

--- server/application/models/Log.php
+++ server/application/models/Log.php
@@ -4,7 +4,7 @@ class Cronphp_Model_Log extends Zend_Db_Table_Abstract {
     protected $_rowClass = 'Cronphp_Model_Log_Row';
 
     public function getLog() {
-        $logs = $this->fetchAll();
+        $logs = $this->fetchAll($this->select()->order('start DESC'));
 
         return $logs;
     }
